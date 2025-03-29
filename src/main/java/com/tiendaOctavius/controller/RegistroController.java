b64c4e106483f7e34ced9e0bb482fef8ea8e643f
Ed3R08 <ederseva@gmail.com>
2025-03-29 11:17:11 -0600

    Registro de usuarios y login, falta meter informacion a la base de datos

--- src/main/java/com/tiendaOctavius/controller/RegistroController.java
+++ src/main/java/com/tiendaOctavius/controller/RegistroController.java
@@ -1,31 +1,69 @@
 package com.tiendaOctavius.controller;
 
 import com.tiendaOctavius.domain.Usuario;
-import com.tiendaOctavius.service.UsuarioService;
+import com.tiendaOctavius.service.RegistroService;
+import jakarta.mail.MessagingException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
+@Slf4j
 @RequestMapping("/registro")
 public class RegistroController {
 
     @Autowired
-    private UsuarioService usuarioService;
+    private RegistroService registroService;
 
-    // Mostrar el formulario de registro
-    @GetMapping
-    public String registroForm(Model model) {
-        model.addAttribute("usuario", new Usuario());
-        return "registro/registro"; // Nombre de la vista (registro.html)
+    @GetMapping("/nuevo")
+    public String nuevo(Model model, Usuario usuario) {
+        return "/registro/nuevo";
     }
 
-    // Procesar el formulario de registro
-    @PostMapping("/guardar")
-    public String guardarUsuario(@ModelAttribute("usuario") Usuario usuario) {
-        // Guardamos el usuario (se encripta la contraseña)
-        usuarioService.save(usuario);
-        return "/login";
+    @GetMapping("/recordar")
+    public String recordar(Model model, Usuario usuario) {
+        return "/registro/recordar";
+    }
+
+    @PostMapping("/crearUsuario")
+    public String crearUsuario(Model model, Usuario usuario) 
+            throws MessagingException {
+        model = registroService.crearUsuario(model, usuario);
+        return "/registro/salida";
+    }
+
+    @GetMapping("/activacion/{usuario}/{id}")
+    public String activar(
+            Model model, 
+            @PathVariable(value = "usuario") String usuario, 
+            @PathVariable(value = "id") String id) {
+        model = registroService.activar(model, usuario, id);
+        if (model.containsAttribute("usuario")) {
+            return "/registro/activa";
+        } else {
+            return "/registro/salida";
+        }
+    }
+
+    @PostMapping("/activar")
+    public String activar(
+            Usuario usuario, 
+            @RequestParam("imagenFile") MultipartFile imagenFile) {
+        registroService.activar(usuario, imagenFile);
+        return "redirect:/";
+    }
+
+    @PostMapping("/recordarUsuario")
+    public String recordarUsuario(Model model, Usuario usuario) 
+            throws MessagingException {
+        model = registroService.recordarUsuario(model, usuario);
+        return "/registro/salida";
     }
 }
--- src/main/java/com/tiendaOctavius/service/UsuarioService.java
+++ src/main/java/com/tiendaOctavius/service/UsuarioService.java
@@ -1,29 +1,66 @@
 package com.tiendaOctavius.service;
 
+import com.tiendaOctavius.domain.Rol;
 import com.tiendaOctavius.domain.Usuario;
+import com.tiendaOctavius.repository.RolRepository;
 import com.tiendaOctavius.repository.UsuarioRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UsuarioService {
-    
-    @Autowired
+@Autowired
     private UsuarioRepository usuarioRepository;
-    
-    // Spring Security inyectará un PasswordEncoder (BCrypt)
+   //Se incorpora RolService porque cuando se crea un usuario... tambien se crea un rol user
+
     @Autowired
-    private PasswordEncoder passwordEncoder;
-    
-    // Método para registrar/guardar un usuario
-    public void save(Usuario usuario) {
-        // Encriptamos la contraseña antes de guardar
-        usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
-        usuarioRepository.save(usuario);
+    private RolRepository rolRepository;
+
+    @Transactional(readOnly = true)
+    public List<Usuario> getUsuarios() {
+        return usuarioRepository.findAll();
     }
-    
-    public Usuario getUsuarioByUsername(String username) {
+
+    @Transactional(readOnly = true)
+    public Usuario getUsuario(Usuario usuario) {
+        return usuarioRepository.findById(usuario.getIdUsuario()).orElse(null);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario getUsuarioPorUsername(String username) {
         return usuarioRepository.findByUsername(username);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario getUsuarioPorUsernameYPassword(String username, String password) {
+        return usuarioRepository.findByUsernameAndPassword(username, password);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario getUsuarioPorUsernameOCorreo(String username, String correo) {
+        return usuarioRepository.findByUsernameOrCorreo(username, correo);
+    }
+
+    @Transactional(readOnly = true)
+    public boolean existeUsuarioPorUsernameOCorreo(String username, String correo) {
+        return usuarioRepository.existsByUsernameOrCorreo(username, correo);
+    }
+
+    @Transactional
+    public void save(Usuario usuario, boolean crearRolUser) {
+        usuario=usuarioRepository.save(usuario);
+        if (crearRolUser) {  //Si se está creando el usuario, se crea el rol por defecto "USER"
+            Rol rol = new Rol();
+            rol.setNombre("USER");
+            rol.setIdUsuario(usuario.getIdUsuario());
+            rolRepository.save(rol);
+        }
+    }
+
+    @Transactional
+    public void delete(Usuario usuario) {
+        usuarioRepository.delete(usuario);
     }
 }
